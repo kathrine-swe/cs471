7737b6074662f24953675cfd0b52ee44edcceb53
Kathrine Sweebe <kathrine.swe@Kathrines-MacBook-Air.local>
2019-09-09 18:41:45 -0600

    Adding Comments

--- hw2/ShortCircuit.java
+++ hw2/ShortCircuit.java
@@ -1,6 +1,15 @@
 /* Kay Sweebe
  * CS471 - S Cooper
- * Short Circuiting in Java
+ * Programming2 - Short Circuit
+ *
+ * Demonstrate whether the Java programming language utilizes the concept of
+ * short circuiting.
+ *
+ * Short circuiting occurs when only part of an expression needs to be
+ * evaluated before finding the value.
+ * See the following rules:
+ *  if a and b and not a then false
+ *  if a or  b and a     then true
  */
 
 public class ShortCircuit {
@@ -19,10 +28,17 @@ public class ShortCircuit {
   }// end test_short_circuit
 
   public static void main(String[] args){
-    boolean x = false; // First condition should fail
-    System.out.println("Start short circuit test...");
+    boolean x = false; // First condition should fail for and
+    boolean y = true; // First condition should pass for or
+
+    System.out.println("Start short circuit test AND...");
     if( x && test_short_circuit() );
     System.out.println("...End short circuit test");
+
+    System.out.println("Start short circuit test OR...");
+    if( y || test_short_circuit() );
+    System.out.println("...End short circuit test");
+
     System.out.println("Helper function never prints");
     System.out.println("Short circuiting is used in Java");
   }// end main
